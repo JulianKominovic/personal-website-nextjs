97bbaa8b24d7abd33749171b0f7dc2cb1dc12a52
julian <julian@julians-MacBook-Pro.local>
2024-01-19 19:55:12 -0300

    feat: mobile navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 import { useEffect } from "react";
 
 import { Inter as FontSans } from "next/font/google";
+import Navbar from "@/layout/header/Navbar";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -80,7 +81,7 @@ export default function RootLayout({
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
           </div>
         </div>
-        <Header />
+        <Navbar />
         <main id="main" className="overflow-x-hidden">
           {children}
         </main>
--- src/layout/header/Navbar.tsx
+++ src/layout/header/Navbar.tsx
@@ -1,36 +1,54 @@
 "use client";
-import MagneticButton from "@/components/MagneticButton";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-import Image from "next/image";
-import { useEffect, useRef } from "react";
-const menuLinks = [
-  {
-    title: "Home",
-    path: "/",
-  },
-  {
-    title: "Blog",
-    path: "/posts/",
-  },
-  {
-    title: "AI",
-    path: "/ai/",
-  },
-  {
-    title: "Resources",
-    path: "/resources/",
-  },
-];
+import { usePathname } from "next/navigation";
+
+import { PAGE_INDEX, SOCIAL_LINKS } from "@/const";
+import Link from "next/link";
+import { Dot } from "lucide-react";
+import clsx from "clsx";
 
 export default function Navbar() {
-  return <nav></nav>;
+  const pathname = usePathname();
+  return (
+    <nav className="fixed z-50 max-w-full p-px mx-auto overflow-hidden overflow-x-scroll -translate-x-1/2 rounded-full w-fit sm:mx-0 bottom-1 bg-gradient-to-t from-transparent to-foreground/40 left-1/2 sm:left-4 sm:top-4 sm:w-14">
+      <ul className="flex items-center justify-center h-full px-2 pt-1 pb-3 overflow-hidden rounded-full w-fit sm:px-0 sm:flex-col bg-background bg-gradient-to-t from-background to-foreground/10">
+        {Object.values(PAGE_INDEX).map(({ icon, path, title }) => (
+          <li
+            key={path}
+            className="flex justify-center w-10 h-10 p-1 aspect-square"
+          >
+            <Link
+              href={path}
+              aria-selected={pathname === path}
+              className={clsx(
+                "flex flex-col items-center justify-center w-10 h-10 p-1 rounded-full aspect-square hover:bg-foreground/10 aria-selected:text-foreground text-muted-foreground relative"
+              )}
+            >
+              {icon}
+              {pathname === path && (
+                <Dot className="absolute -translate-x-1/2 -bottom-2 left-1/2" />
+              )}
+            </Link>
+          </li>
+        ))}
+        <li className="flex justify-center w-px h-full p-1 px-2 mt-1 sm:w-full sm:h-px aspect-square">
+          <hr className="border" />
+        </li>
+        {SOCIAL_LINKS.map(({ link, friendlyName, icon, label }) => (
+          <li
+            key={link}
+            className="flex justify-center w-10 h-10 p-1 aspect-square"
+          >
+            <a
+              href={link}
+              className={clsx(
+                "flex flex-col items-center justify-center w-10 h-10 p-1 rounded-full aspect-square hover:bg-foreground/10 aria-selected:text-foreground text-muted-foreground relative"
+              )}
+            >
+              {icon}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
